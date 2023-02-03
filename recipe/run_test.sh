a3de9664c8f46633a4674b5e0cb1b227c375e5f1
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-02-02 21:09:49 -0700

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,2 +1,5 @@
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_rmaps_base_oversubscribe=yes
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
 python "test/test-meshes.py"
 python "test/test-mesh-generation.py"
